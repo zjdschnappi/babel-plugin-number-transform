fdce53b6cf5e81c3c3927f05c312649384308ca1
zhangjiandong <jdzhang@creams.io>
2021-01-05 20:50:57 +0800

    feat: 将识别的含操作符的代码信息存入文件

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import BabelCore, { PluginObj } from "@babel/core";
+import generator from "@babel/generator";
+const fs = require("fs");
 export default function ({ types }: typeof BabelCore): PluginObj<any> {
   return {
     name: "babel-plugin-number-transform",
     visitor: {
       BinaryExpression(path, state) {
         const { node } = path;
-        const { left, right, operator } = node;
+        const { left, right, operator, loc } = node;
         if (
-          (operator === "*" ||
+          ((operator === "*" ||
             operator === "+" ||
             operator === "-" ||
             operator === "/") &&
-          types.isMemberExpression(left)
+            types.isOptionalMemberExpression(left) &&
+            !types.isStringLiteral(right)) ||
+          (types.isMemberExpression(left) &&
+            !types.isStringLiteral(right) &&
+            (operator === "*" ||
+              operator === "+" ||
+              operator === "-" ||
+              operator === "/"))
         ) {
-          console.log(left.property);
-
-          console.log("匹配成功", state.file.opts.filename);
+          if (!state.file.opts.filename.includes("mock/")) {
+            fs.appendFile(
+              "demo.txt",
+              `${state.file.opts.filename}:${generator(node).code}\n`,
+              () => {}
+            );
+          }
         }
       },
     },
